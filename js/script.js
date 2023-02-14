cf744ad130d314b603edb336cc329d7db92e792a
Agata Mikulska <amikulska735@gmail.com>
2023-02-14 17:08:30 +0100

    changed script

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-{
+{   
     const calculateResult = (amount, currency) => {
         const GBP = 5.3415;
         const USD = 4.3133;
@@ -13,46 +13,31 @@
     
             default:
                 return amount * EUR;
-        }
-    
-    }
-    
+}};
     const onFormSubmit = (event) => {
         event.preventDefault();
-    
         const currencyElement = document.querySelector(".js-currency");
         const amountElement = document.querySelector(".js-amount");
         const resultElement = document.querySelector(".js-result");
-    
         const amount = +amountElement.value;
         const currency = currencyElement.value;
-    
         const result = calculateResult(amount, currency);
-    
-        resultElement.innerHTML = `${result.toFixed(2)}`
-    }
-    
-    const init = () => {
-        const formElement = document.querySelector(".js-form");
-    
-    formElement.addEventListener("submit", onFormSubmit);
+        resultElement.innerHTML = `${result.toFixed(2)}`;
     };
-    init();
-    
     const toggleBackground = () => {
         const bodyElement = document.querySelector(".js-body");
         const spanElement = document.querySelector(".js-span"); 
-    
         bodyElement.classList.toggle("body__image");
         spanElement.innerText = bodyElement.classList.contains("body__image") ?
         "usuń" : "dodaj";
     };
-    const toggle = () => {
+    const init = () => {
+        const formElement = document.querySelector(".js-form");
+        formElement.addEventListener("submit", onFormSubmit);
         const buttonBackground = document.querySelector(".js-button--background");
         buttonBackground.addEventListener("click", toggleBackground);
-    
     };
-    toggle();
+    init();
 }
 
 
